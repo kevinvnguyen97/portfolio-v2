e9ea29498b1a062153f2f01ed651248a47899423
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2024-11-22 09:43:41 -0600

    updated bio and added location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,20 +7,32 @@ import {
   Link,
   Image,
   Flex,
+  Text,
 } from "@chakra-ui/react";
-import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin, AiFillGithub, AiOutlineGlobal } from "react-icons/ai";
 import { Links } from "../constants/links";
+
+const BIO = `Hello! I am Kevin. I am currently a software engineer with three
+            years of professional development experience and a Bachelors of Science degree from Lewis University.
+            Here I present some of my pet projects to demonstrate my skills.`;
 
 export const Home = () => {
   return (
     <Box alignContent="center">
       <Flex
         justifyContent="space-evenly"
+        alignItems="center"
         flexDirection={{ smToMd: "column", mdTo2xl: "row" }}
+        gap={5}
       >
-        <VStack>
+        <VStack maxWidth={500}>
           <Heading size="5xl">Kevin Nguyen</Heading>
-          <Heading size="xl">Full Stack React Developer</Heading>
+          <Heading size="2xl">Full Stack React Developer</Heading>
+          <HStack>
+            <AiOutlineGlobal />
+            <Heading size="xl">Chicago, IL USA</Heading>
+          </HStack>
+          <Text textAlign="center">{BIO}</Text>
           <HStack>
             <Link href={Links.LINKEDIN} target="_blank">
               <IconButton aria-label="Linkedin" variant="ghost" size="2xl">
