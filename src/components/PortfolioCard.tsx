23248dfb1d18cf5f5a9bcf4d4aff5bd4037d8b95
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2024-12-12 11:07:46 -0600

    portfolio card is own component

---
+++ src/components/PortfolioCard.tsx
@@ -0,0 +1,61 @@
+import {
+  Card,
+  Box,
+  CardHeader,
+  Heading,
+  Button,
+  Image,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+import { AdventureRoutesIcon } from "./AdventureRoutesIcon";
+
+type PortfolioCardProps = {
+  title: string;
+  description: string;
+  technologiesUsed: string;
+  repoUrl: string;
+  appUrl: string;
+  appImage: string;
+};
+export const PortfolioCard = (props: PortfolioCardProps) => {
+  const { title, description, technologiesUsed, repoUrl, appUrl, appImage } =
+    props;
+  return (
+    <Card.Root flexDirection="row" maxW={{ smToLg: "100%", lg: "1/2" }}>
+      <Image src={appImage} maxW={200} bgColor="orange" />
+      <Box>
+        <CardHeader>
+          <Heading>{title}</Heading>
+        </CardHeader>
+        <Card.Body>
+          <Text>{description}</Text>
+          <br />
+          <Text fontWeight="bold">Technologies Used</Text>
+          <Text>{technologiesUsed}</Text>
+        </Card.Body>
+        <Card.Footer>
+          <Link
+            href={repoUrl}
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Button variant="subtle">
+              <AiFillGithub /> Repo
+            </Button>
+          </Link>
+          <Link
+            href={appUrl}
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Button variant="subtle">
+              <AdventureRoutesIcon /> App
+            </Button>
+          </Link>
+        </Card.Footer>
+      </Box>
+    </Card.Root>
+  );
+};
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,63 +1,18 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardHeader,
-  Heading,
-  Image,
-  Link,
-  Text,
-} from "@chakra-ui/react";
-import { AiFillGithub } from "react-icons/ai";
-import { AdventureRoutesIcon } from "../components/AdventureRoutesIcon";
+import { Box } from "@chakra-ui/react";
+import { PortfolioCard } from "../components/PortfolioCard";
 import { Links } from "../constants/links";
 
 export const Portfolio = () => {
   return (
     <Box>
-      <Card.Root flexDirection="row" maxW={{ smToLg: "100%", lg: "1/2" }}>
-        <Image
-          src="/images/adventure_routes_icon.png"
-          maxW={200}
-          bgColor="orange"
-        />
-        <Box>
-          <CardHeader>
-            <Heading>Adventure Routes</Heading>
-          </CardHeader>
-          <Card.Body>
-            <Text>
-              This application saves routes logged by any roadgeek or traveler.
-            </Text>
-            <br />
-            <Text fontWeight="bold">Technologies Used</Text>
-            <Text>
-              Typescript, Google Maps API, ReactJS, MeteorJS, MongoDB, Chakra UI
-              v2
-            </Text>
-          </Card.Body>
-          <Card.Footer>
-            <Link
-              href={`${Links.GITHUB}/adventure-routes-v3`}
-              target="_blank"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Button variant="subtle">
-                <AiFillGithub /> Repo
-              </Button>
-            </Link>
-            <Link
-              href={Links.ADVENTURE_ROUTES}
-              target="_blank"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Button variant="subtle">
-                <AdventureRoutesIcon /> App
-              </Button>
-            </Link>
-          </Card.Footer>
-        </Box>
-      </Card.Root>
+      <PortfolioCard
+        title="Adventure Routes"
+        description="This application saves routes logged by any roadgeek or traveler."
+        technologiesUsed="Typescript, Google Maps API, ReactJS, MeteorJS, MongoDB, Chakra UI v2"
+        repoUrl={`${Links.GITHUB}/adventure-routes-v3`}
+        appUrl={Links.ADVENTURE_ROUTES}
+        appImage="/images/adventure_routes_icon.png"
+      />
     </Box>
   );
 };
