0d46a6f14cbd2b1fd5ccaa4dc899d08b4f980eb9
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2024-12-14 22:30:15 -0600

    import fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "/App";
-import reportWebVitals from "/reportWebVitals";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
-import { ColorModeProvider } from "/components/ui/color-mode";
+import { ColorModeProvider } from "./components/ui/color-mode";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
